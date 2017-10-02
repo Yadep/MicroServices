a52730969b2d52de92a820e7507aff30786ac7e8
Yadep <aldwin49@gmail.com>
2017-10-02 15:23:09 +0200

    Ajout de commentaires

--- Pollution/PollutionByCity.php
+++ Pollution/PollutionByCity.php
@@ -6,19 +6,24 @@
  * and open the template in the editor.
  */
 
+//Récupération des données depuis l'API prevair
 $json = file_get_contents('http://www2.prevair.org/ineris-web-services.php?url=atmo&date=20171002');
-
+//Création d'un objet JSON récuperer au dessus
 $jsonextract = json_decode($json);
-//var_dump($jsonextract);
 
+//On parcoure la liste contenu dans le json, ville par ville
 foreach($jsonextract as $j){
+    //Si la ville est ANGERS
     if($j[4] == "ANGERS"){
+        //On récupere incide et le pm10 que l'on stock dans des variables
         $incide = $j[7];
         $pm10 = $j[11];
     }     
 }
 
+//Instance d'une classe pour créer un JSON
 $obj = new stdClass();
+//On ajoute les variables récuperer précédemment
 $obj->data = array(
      array(
         "Ville", "ANGERS"
@@ -30,7 +35,7 @@ $obj->data = array(
         "PM10", $pm10
     )
 );
-
+//On retourne le JSON encoder
 echo json_encode($obj);
 
 
--- Weather/TempByCity.php
+++ Weather/TempByCity.php
@@ -5,18 +5,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//Si l'on souhaite rendre le parametre ville dynamique, il faut décommenter
 //Parametre de la ville
-$ville = $_GET['ville'] ;
+//$ville = $_GET['ville'] ;
 //$json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q='.$ville.'&APPID=615d9b76eeae8b87d336cd9bc9bc2872'); 
+//Récupération des données depuis l'API openweathermap
 $json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Angers&APPID=615d9b76eeae8b87d336cd9bc9bc2872'); 
+//Création d'un objet JSON récuperer au dessus
 $jsonextract = json_decode($json);
 
+//Recupération de la temperature et conversion K vers degrès celcius
 $temp = $jsonextract->main->temp -  273.15;
+//Récupération de l'icon (condition)
 $condition = $jsonextract->weather[0]->icon ;
+//cast de l'url pour la condition
 $urlCondition =  'http://openweathermap.org/img/w/'. $condition. ".png";
 
+//Instance d'une classe pour créer un JSON
 $obj = new stdClass();
-//$obj->label="Temp by city";
+//On ajoute les variables récuperer précédemment
 $obj->data = array(
  //   array('ville',$ville),
     array(
@@ -26,7 +34,7 @@ $obj->data = array(
     array('urlIcon',$urlCondition),
     //and so on...
 );
-
+//On retourne le JSON encoder
 echo utf8_decode(json_encode($obj));
 
 
