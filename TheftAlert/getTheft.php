5502f58c6d9f8ee55c34283f979967f6cd2c7303
Yadep <aldwin49@gmail.com>
2017-10-03 10:16:02 +0200

    Ajout de PostTheft

--- TheftAlert/getTheft.php
+++ TheftAlert/getTheft.php
@@ -1,3 +1,3 @@
 <?php
-
+        $connexion = new PDO("mysql:host=localhost;dbname=theftalert","root",""); // connexion a la BDD
 ?>
--- TheftAlert/postTheft.php
+++ TheftAlert/postTheft.php
@@ -1,8 +1,15 @@
 <?php
         $connexion = new PDO("mysql:host=localhost;dbname=theftalert","root",""); // connexion a la BDD
-        $sql = $connexion->prepare("INSERT INTO theftalert (longitude,latitude,datevol) VALUES (".$_POST['longitude'].", ".$_POST['latitude'].", ".$_POST['datevol'].")");
-        $sql->execute();
+        $sql = $connexion->prepare("INSERT INTO theftalert (longitude,latitude,datevol) VALUES (:longitude, :latitude, :datevol)");
+        $sql->execute(array(
+            "longitude"=>$_POST['longitude'],
+            "latitude"=>$_POST['latitude'],
+            "datevol"=>$_POST['datevol']
+        ));
         $nb_ligne = $sql->rowCount();
 
+        
+        print_r($sql->errorInfo());
+        echo $nb_ligne;
 echo "INSERT INTO theftalert (longitude,latitude,datevol) VALUES (".$_POST['longitude'].", ".$_POST['latitude'].", '".$_POST['datevol']."')";
 ?>
